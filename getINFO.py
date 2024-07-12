ffdd0079ebf1b0eac210e24203f74315c1a390a7
michaelisrael1 <misrael740@gmail.com>
2024-07-12 15:30:31 -0500

    Updated getINFO fuctions

---
+++ getINFO.py
@@ -0,0 +1,35 @@
+from demographics import *
+
+
+def input_demographics():
+    name = input("What is your name? ").strip()
+    age = input("What is your age? ").strip()
+    sex = input("What is your sex?(M/F): ").lower().strip()
+    weight = input("What is your weight?(lb): ").strip()
+    goal_weight = input("What is your goal weight?(lb): ").strip()
+
+    return name, age, sex, weight, goal_weight
+
+def get_goals(profile):
+    print("""
+    1. Gain weight
+    2. Lose weight
+    3. Maintain weight
+    """)
+    weight = profile['weight']
+    maintain = weight*15
+    goal = input("What is your goal? ").strip()
+    while goal not in ['g', 'l', 'm']:
+        goal = input("What is your goal? ").strip()
+
+    if goal == 'g':
+        pounds = input("How many pounds do you want to gain a week?(0-2): ").strip()
+        calories_per_day = maintain + (((float(pounds))/2)*1000)
+    elif goal == 'l':
+        pounds = input("How many pounds do you want to lose a week?(0-2): ").strip()
+        calories_per_day = maintain - (((float(pounds)) / 2)*1000)
+    elif goal == 'm':
+        calories_per_day = maintain
+
+
+    return goal, calories_per_day
